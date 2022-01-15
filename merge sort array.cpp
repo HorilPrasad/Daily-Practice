8d5d6d7cdcea13906174e79f87b6b26a83e5d6c8
Horil Kumar <horil.2021ca045@mnnit.ac.in>
2022-01-15 19:15:17 +0530

    solved

---
+++ merge sort array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m;
+        int j=0;
+        vector<int> res;
+        while(j<n)
+        {
+            nums1[i]=nums2[j];
+            i++;
+            j++;
+        }
+        
+       sort(nums1.begin(),nums1.end());
+        
+    
+    }
+};
